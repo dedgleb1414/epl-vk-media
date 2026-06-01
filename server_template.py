20de3970d43c8e42b277e89ce5bf4a936d960ccd
DedGleb1414 <dedgleb1414@gmail.com>
2026-06-01 09:34:47 +0300

    token from env

--- server_template.py
+++ server_template.py
@@ -4,7 +4,8 @@ from services.football_api import get_matches, get_upcoming_matches, get_leagues
 from services.image_search import find_match_image
 
 app = Flask(__name__)
-CONFIRMATION_TOKEN = "0a1370a2"
+import os
+CONFIRMATION_TOKEN = os.environ.get("CONFIRMATION_TOKEN", "0a1370a2")
 
 state = {}
 drafts = {}
